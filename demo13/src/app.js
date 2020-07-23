ac363a4365297a09901cebd5f5b20c23f6df9e9b
550185271 <550185271@qq.com>
2020-07-23 21:37:10 +0800

    箭头函数；

--- demo13/src/app.js
+++ demo13/src/app.js
@@ -4,7 +4,7 @@ export default class App extends React.Component{
 
   constructor(props) {
     super(props);
-    this.render = this.render.bind(this);
+    // this.render = this.render.bind(this);
     this.state = {
       items: this.props.items,
       disabled: true
@@ -17,7 +17,7 @@ export default class App extends React.Component{
     })
   }
 
-  handleClick() {
+  handleClick = () => {
     this.setState({
       items: this.state.items.concat('Item ' + this.state.items.length)
     })
@@ -26,7 +26,7 @@ export default class App extends React.Component{
   render() {
     return (
       <div>
-        <button onClick={this.handleClick.bind(this)} disabled={this.state.disabled}>Add Item</button>
+        <button onClick={this.handleClick} disabled={this.state.disabled}>Add Item</button>
         <ul>
         {
           this.state.items.map(function(item, index) {
